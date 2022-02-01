184e64f49a64990acd46fdc3b7f075eb6426f823
Marianne Corbel <mariannecorbel@hotmail.fr>
2022-02-01 22:15:18 +0100

    Fixed issue on "add" + "edit"

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,7 @@ function convertToObject(localObj) {
 }
 
 function displayChecklist() {
+  document.getElementById("demo").className = "demo";
   if (Object.keys(tasks).length == 0) {
     document.getElementById("title").innerHTML = "Nothing to see here!";
   } else {
@@ -52,38 +53,46 @@ function displayChecklist() {
 }
 
 function openEdit() {
-    if (Object.keys(tasks).length != 0) {
-        document.getElementById("title").innerHTML = tasks[0];
-        document.getElementById("demo").innerHTML = "";
-        document.getElementById("demo").classList.remove("demo");
-        document.getElementById("demo").classList.add("updateContainer");
-        var form = document.createElement("form"); form.id = "updateForm"; form.onsubmit="";
-        for (let i = 1; i < (Object.keys(tasks).length); i++){
-            var uCell = document.createElement("div"); uCell.classList.add("updateCell");
-            var delSpan = document.createElement("span"); delSpan.innerHTML = "x";
-            var delButton = document.createElement("div"); delButton.classList.add("deleteButton"); delButton.id = `delButton${i}`;
-            delButton.setAttribute('onclick', `deleteTask(${i})`);
-            delButton.appendChild(delSpan); uCell.appendChild(delButton);
-            var tCell = document.createElement("div"); tCell.classList.add("taskCell");
-            var checkbox = document.createElement("input"); checkbox.type = "checkbox"; checkbox.classList.add("editCheck"); 
-            checkbox.checked = tasks[i].checked; checkbox.id = `checkEdit_${i}`; checkbox.name = checkbox.id;
-            var checkSpan = document.createElement("span"); checkSpan.classList.add("chbx_checkmark");
-            var label = document.createElement("label"); label.classList.add("chbx_container");
+    if (document.getElementById("demo").classList.contains("updateContainer") == false) {
+        if (Object.keys(tasks).length != 0) {
+            document.getElementById('displayAdd').style.display = "none";
+            document.getElementById("title").innerHTML = tasks[0];
+            document.getElementById("demo").innerHTML = "";
+            document.getElementById("demo").classList.remove("demo");
+            document.getElementById("demo").classList.add("updateContainer");
+            var form = document.createElement("form"); form.id = "updateForm"; form.onsubmit="";
+            for (let i = 1; i < (Object.keys(tasks).length); i++){
+                var uCell = document.createElement("div"); uCell.classList.add("updateCell");
+                var delSpan = document.createElement("span"); delSpan.innerHTML = "x";
+                var delButton = document.createElement("div"); delButton.classList.add("deleteButton"); delButton.id = `delButton${i}`;
+                delButton.setAttribute('onclick', `deleteTask(${i})`);
+                delButton.appendChild(delSpan); uCell.appendChild(delButton);
+                var tCell = document.createElement("div"); tCell.classList.add("taskCell");
+                var checkbox = document.createElement("input"); checkbox.type = "checkbox"; checkbox.classList.add("editCheck"); 
+                checkbox.checked = tasks[i].checked; checkbox.id = `checkEdit_${i}`; checkbox.name = checkbox.id;
+                var checkSpan = document.createElement("span"); checkSpan.classList.add("chbx_checkmark");
+                var label = document.createElement("label"); label.classList.add("chbx_container");
 
-            label.appendChild(checkbox); label.appendChild(checkSpan); tCell.appendChild(label);
-            var text = document.createElement("input"); text.type = "text"; text.classList.add("editText"); 
-            text.placeholder="Test"; text.style.width = "100%"; 
-            text.value = tasks[i].name; text.id = `textEdit_${i}`; text.name = text.id;
-            tCell.appendChild(text);
-        
-            uCell.appendChild(tCell); 
-            form.appendChild(uCell);
+                label.appendChild(checkbox); label.appendChild(checkSpan); tCell.appendChild(label);
+                var text = document.createElement("input"); text.type = "text"; text.classList.add("editText"); 
+                text.placeholder="Test"; text.style.width = "100%"; 
+                text.value = tasks[i].name; text.id = `textEdit_${i}`; text.name = text.id;
+                tCell.appendChild(text);
+            
+                uCell.appendChild(tCell); 
+                form.appendChild(uCell);
+            }
+            var submit = document.createElement("input"); submit.type = "button"; 
+            submit.setAttribute('onclick', 'editTasklist(this.form)');
+            submit.id = "updateSubmit"; submit.value = "UPDATE"; 
+            form.appendChild(submit); 
+            document.getElementById("demo").appendChild(form);
         }
-        var submit = document.createElement("input"); submit.type = "button"; 
-        submit.setAttribute('onclick', 'editTasklist(this.form)');
-        submit.id = "updateSubmit"; submit.value = "UPDATE"; 
-        form.appendChild(submit); 
-        document.getElementById("demo").appendChild(form);
+    }
+    else {
+        document.getElementById("demo").innerHTML = "";
+        document.getElementById('displayAdd').style.display = null;
+        displayChecklist();
     }
 }
 

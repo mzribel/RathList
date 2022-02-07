ed9f5441553d8385ff668bfbc52531c0e29c7431
Marianne Corbel <mariannecorbel@hotmail.fr>
2022-02-07 23:39:56 +0100

    New features added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,11 @@ var display_add = document.getElementById("display_add");
 var h1 = document.getElementById("title");
 let tasklist = {};
 let current_tl = '0';
+let id_number = 0;
 
 let temp = [
     [
-        "Improving RathList!",
+        "Improving RathList!", 
         {
             "name":"Dynamic and editable tasklists",
             "checked":true,
@@ -35,7 +36,8 @@ let temp = [
         },
 
     ],
-    [ "Things I'd like to learn/master",
+    [ 
+        "Things I'd like to learn/master",
         {
             "name":"C",
             "checked":true,
@@ -65,7 +67,6 @@ let temp = [
 
 function storageInit() {
     if (localStorage.length == 0) {
-            console.log(temp);
             storageUpdate(temp);
             current_tl = 1; currentUpdate();
     }
@@ -97,7 +98,7 @@ function displayChecklist() {
     // Selected tasklist is empty.
     } else if (tasklist[`${current_tl}`].length == 1) {
         h1.innerHTML = tasklist[`${current_tl}`][0];
-        document.getElementById("p").style.display = 'block';
+        document.getElementById("emptylist").style.display = 'block';
     // Generate tasks of selected tasklist.
     } else {
         let checked_nb = 0;
@@ -129,80 +130,136 @@ function displayChecklist() {
     }
 }
 
-function openEdit() {
-    displayEdit();
-    if (tasklist[`${current_tl}`].length != 0) {
-        // Creates global form and title edit text input.
-        var form = document.createElement("form"); form.id = "updateForm"; form.setAttribute('onsubmit', 'editTasklist(event)'); form.setAttribute('method', 'post');
-        var titleEdit = document.createElement("input"); titleEdit.type = "text"; titleEdit.classList.add("editText", "title", "big_input_txt");
-        titleEdit.value = tasklist[`${current_tl}`][0]; titleEdit.id = 'titleEdit'; titleEdit.name = titleEdit.id;
-        // Appends title edit text input to global form.
-        form.appendChild(titleEdit);
-        // Generates each task edit input.
-        for (let i = 1; i < tasklist[`${current_tl}`].length; i++){
-            // Creates parent div for each task edit.
-            var updateCell = document.createElement("div"); updateCell.classList.add("updateCell"); 
+function generateAddForm(parent_div, list="") {
+    // Creates parent div for each task edit.
+    var updateCell = document.createElement("div"); updateCell.classList.add("updateCell"); updateCell.id = `updateCell_${id_number}`;
 
-            // Generates individual delete button and appends to parent div.
-            var delMark = document.createElement("span"); delMark.innerHTML = "x";
-            var delButton = document.createElement("div"); delButton.classList.add("delete_button"); delButton.id = `delButton${i}`;
-            delButton.setAttribute('onclick', `deleteTask(${i})`);
-            updateCell.appendChild(delButton); delButton.appendChild(delMark); 
+    // Generates individual delete button and appends to parent div.
+    var delMark = document.createElement("span"); delMark.innerHTML = "x";
+    var delButton = document.createElement("div"); delButton.classList.add("delete_button"); delButton.id = `delButton${id_number}`;
+    delButton.setAttribute('onclick', `deleteTask(${id_number})`);
+    updateCell.appendChild(delButton); delButton.appendChild(delMark); 
+    
+    // BUTTONS TO CHANGE
+    var order = document.createElement("div"); order.classList.add("order");
+    var plus = document.createElement("a"); plus.innerHTML = "+"; plus.setAttribute("onclick", `invertValues(${id_number}, 'up')`);
+    var moins = document.createElement("a"); moins.innerHTML = "-"; moins.setAttribute("onclick", `invertValues(${id_number}, 'down')`);
+    order.appendChild(plus); order.appendChild(moins); 
+    
+    // Creates child div for each task text input.
+    var taskCell = document.createElement("div"); taskCell.classList.add("taskCell");
 
-            // Creates child div for each task text input.
-            var taskCell = document.createElement("div"); taskCell.classList.add("taskCell");
+    // Creates checkbox container, checkbox and checkmark.
+    var chbx_container = document.createElement("label"); chbx_container.classList.add("chbx_container");
+    var checkbox = document.createElement("input"); checkbox.type = "checkbox"; checkbox.classList.add("check"); 
+    checkbox.id = `checkEdit_${id_number}`; checkbox.name = checkbox.id;
+    var checkmark = document.createElement("span"); checkmark.classList.add("checkmark");
+    
+    // Appends each to child div.
+    chbx_container.appendChild(checkbox); chbx_container.appendChild(checkmark); taskCell.appendChild(chbx_container);
 
-            // Creates checkbox container, checkbox and checkmark.
-            var chbx_container = document.createElement("label"); chbx_container.classList.add("chbx_container");
-            var checkbox = document.createElement("input"); checkbox.type = "checkbox"; checkbox.classList.add("editCheck"); 
-            checkbox.checked = tasklist[`${current_tl}`][i].checked; checkbox.id = `checkEdit_${i}`; checkbox.name = checkbox.id;
-            var checkmark = document.createElement("span"); checkmark.classList.add("checkmark");
+    // Creates text input for task name edit.
+    var text = document.createElement("input"); text.type = "text"; text.classList.add("editText", "small_input_txt", "pouet"); 
+    text.placeholder="Test"; text.style.width = "100%"; 
+    text.id = `textEdit_${id_number}`; text.name = text.id;
 
-            // Appends each to child div.
-            chbx_container.appendChild(checkbox); chbx_container.appendChild(checkmark); taskCell.appendChild(chbx_container);
-
-            // Creates text input for task name edit.
-            var text = document.createElement("input"); text.type = "text"; text.classList.add("editText", "small_input_txt"); 
-            text.placeholder="Test"; text.style.width = "100%"; 
-            text.value = tasklist[`${current_tl}`][i].name; text.id = `textEdit_${i}`; text.name = text.id;
-
-            // Appends text input to child div, child div to parent div and parent div to form.
-            taskCell.appendChild(text); updateCell.appendChild(taskCell); form.appendChild(updateCell);
-        }
-
-        // Generates submit button and appends it to form.
-        var submit = document.createElement("input"); submit.type = "submit"; 
-        submit.id = "updateSubmit"; submit.value = "UPDATE"; 
-        form.appendChild(submit); 
-
-        // Appends form to main div.
-        display_edit.appendChild(form);
+    if (list !== "") {
+        text.value = list.name; checkbox.checked = list.checked;
+    } else {
+        text.value = ""; checkbox.checked = false;
     }
+
+    // Appends text input to child div, child div to parent div and parent div to form.
+    taskCell.appendChild(text); taskCell.appendChild(order); updateCell.appendChild(taskCell); 
+    document.getElementById(parent_div).appendChild(updateCell);
+
+    id_number++;
 }
+
+
+function openEdit(list="") {
+    console.log(list);
+    displayEdit();
+    id_number = 0; let temp = [];
+
+    // Creates global form and title edit text input.
+    var form = document.createElement("form"); form.id = "updateForm"; form.setAttribute('method', 'post');
+    var titleEdit = document.createElement("input"); titleEdit.type = "text"; titleEdit.classList.add("editText", "title", "big_input_txt");
+    titleEdit.id = 'titleEdit'; titleEdit.name = titleEdit.id;
+    
+    // Appends form to main div.
+    display_edit.appendChild(form);
+    var subtitle = document.createElement("div"); subtitle.classList.add("subtitle");
+    var a = document.createElement("a"); a.setAttribute("onclick", `generateAddForm("container")`); a.innerHTML = "Add a task."; 
+    var b = document.createElement("a"); b.setAttribute("onclick", `deleteTasklist(${current_tl})`); b.innerHTML = "Delete tasklist."; 
+
+    subtitle.appendChild(a); subtitle.appendChild(b);
+    form.appendChild(titleEdit); form.appendChild(subtitle);
+    
+    var container = document.createElement("div"); container.id = "container"; form.appendChild(container);
+
+    if (list !== "") {
+        temp = [...list]; title = temp.shift(); titleEdit.value = title;
+        form.setAttribute('onsubmit', `editTasklist(event, tasklist[${current_tl}])`); 
+        if (temp.length == 0) {
+            generateAddForm("container");
+        } else {
+            // Generates each task edit input.
+            for (task of temp) {
+                // Creates parent div for each task edit.
+                generateAddForm("container", task)
+            }
+        }    
+    } else {
+        form.setAttribute('onsubmit', `editTasklist(event)`); 
+        title = "New list"; titleEdit.value = title;
+        for (let i = 0; i < 5; i++) {
+            generateAddForm("container");
+        }
+    }
+    var submit = document.createElement("input"); submit.type = "submit"; 
+    submit.id = "updateSubmit"; submit.value = "UPDATE"; 
+    form.appendChild(submit);
+}
+
 
 // --------------------------------------------//
 // ------ ADD / DELETE / EDIT FUNCTIONS ------ // 
 // --------------------------------------------//
 
-function editTasklist(event) {
+function editTasklist(event, list="") {
+    console.log(list);
     event.preventDefault();
     let form = event.currentTarget;
-    console.log(form);
-    if (form['titleEdit'].value != "") {
-        tasklist[`${current_tl}`][0] = form['titleEdit'].value;
+
+    let temp_list = [];
+    if (form['titleEdit'].value == "") {
+        temp_list[0] = list[0];
+    } else {
+        temp_list[0] = form['titleEdit'].value;
     }
-    for (let i = 1; i < tasklist[`${current_tl}`].length; i++){
-        if (form[`textEdit_${i}`].value != "") {
-            tasklist[`${current_tl}`][i] = {
-                "name": form[`textEdit_${i}`].value,
-                "checked": form[`checkEdit_${i}`].checked,
-            }
+
+    let form_tasks = form.getElementsByClassName("pouet");
+    for (task of form_tasks) {
+        if (task.value != "") {
+            id = task.id.slice(-1);
+
+            let temp = {};
+            temp["name"] = form[`textEdit_${id}`].value;
+            temp["checked"] = form[`checkEdit_${id}`].checked;
+            temp_list.push(temp);
         }
     }
-    storageUpdate(tasklist);
+    if (list == "") {
+        tasklist.push(temp_list); storageUpdate(tasklist);
+        current_tl = tasklist.indexOf(temp_list); currentUpdate();
+    } else {
+        tasklist[`${current_tl}`] = temp_list; storageUpdate(tasklist);
+    }
     displayMain(); displayChecklist(); changeList();
-
 }
+
+
 function addTask(event) {
     event.preventDefault();
     let form = event.currentTarget;
@@ -217,19 +274,26 @@ function addTask(event) {
         displayChecklist(); displayAdd();
     }
 }
-function deleteTask(task_id) {
-    tasklist[`${current_tl}`].splice(task_id, 1);
-    storageUpdate(tasklist); countChecked(tasklist[`${current_tl}`], `list_checked_${current_tl}`, `list_${current_tl}`);
-    openEdit();
+function deleteTask(id) {
+    let container = document.getElementById(`updateCell_${id}`); 
+    container.remove();
+}
+function deleteTasklist(id) {
+    console.log(id);
+    tasklist.splice(id, 1); storageUpdate(tasklist);
+    if (id == current_tl) {
+        current_tl = 0; currentUpdate();
+    }
+    displayChecklist(); changeList();
 }
 
 function updateCheck(prout) {
     task = document.getElementById(prout);
     id = prout.slice(-1);
     tasklist[`${current_tl}`][id].checked = task.checked;
-    console.log(id);
     storageUpdate(tasklist); countChecked(tasklist[`${current_tl}`], `list_checked_${current_tl}`, `list_${current_tl}`);
 }
+
 
 
 // --------------------------------------------//
@@ -237,7 +301,8 @@ function updateCheck(prout) {
 // --------------------------------------------//
 
 function displayMain() {
-    h1.style.display = "block"
+    h1.style.display = "block";
+    document.getElementById('order_by').style.display = "block";
     display_main.style.display = "flex"; display_main.innerHTML = "";
     display_edit.style.display = "none"; display_edit.innerHTML = "";
     display_add.style.display = 'none';
@@ -245,6 +310,7 @@ function displayMain() {
 }
 function displayEdit() {
     h1.style.display = "none"; h1.innerHTML = "";
+    document.getElementById('order_by').style.display = "none";
     display_main.style.display = 'none';    display_main.innerHTML = "";
     display_edit.style.display = "flex"; display_edit.innerHTML = "";
     document.getElementById("add_button").style.display = "none";
@@ -253,26 +319,26 @@ function displayEdit() {
 }
 function displaySwitch() {
     if (display_main.style.display == "flex") {
-        openEdit();
+        openEdit(tasklist[`${current_tl}`]);
     } else if (display_edit.style.display == "flex") {
         displayChecklist();
     }
 }
 function Display_Hide_List() {
-    document.getElementById("changeList").innerHTML = "";
-    if (document.getElementById("changeList").style.display == 'none') {
-        document.getElementById("changeList").style.display = "block";
+    document.getElementById("side_bar_tasklists").innerHTML = "";
+    if (document.getElementById("side_bar_tasklists").style.display == 'none') {
+        document.getElementById("side_bar_tasklists").style.display = "block";
         changeList();
     }
     else {
-        document.getElementById("changeList").style.display = "none";
+        document.getElementById("side_bar_tasklists").style.display = "none";
     }
 }
 function Display_Hide_Links() {
-    if (document.getElementById("links").style.display == 'none') {
-        document.getElementById("links").style.display = 'block';
+    if (document.getElementById("socials").style.display == 'none') {
+        document.getElementById("socials").style.display = 'block';
     } else {
-        document.getElementById("links").style.display = 'none';
+        document.getElementById("socials").style.display = 'none';
     }
 }
 
@@ -292,10 +358,9 @@ function displayAdd() {
 // --------------------------------------------//
 
 function changeList() {
-    document.getElementById('changeList').innerHTML = "";
+    document.getElementById('side_bar_tasklists').innerHTML = "";
     if (tasklist.length != 0) {
         for (let i = 0; i < tasklist.length; i++) {
-
             // Creates parent div to both list name and check count.
             var parent_element = document.createElement("div"); parent_element.id = `list_${i}`;
             parent_element.classList.add("lists");
@@ -306,15 +371,23 @@ function changeList() {
             list_name.innerHTML = tasklist[i][0];
             // Appends elements.
             parent_element.appendChild(list_name);
-            document.getElementById('changeList').append(parent_element);
+            document.getElementById('side_bar_tasklists').append(parent_element);
             // Generates count check.
             countChecked(tasklist[i], `list_checked_${i}`, `list_${i}`);
         }
     }
+    
+    var add_new = document.createElement("div"); add_new.classList.add("add_new");
+    add_new.setAttribute('onclick', 'openEdit()'); add_new.innerHTML = "Add a new tasklist..."
+    document.getElementById('side_bar_tasklists').appendChild(add_new);
+
+    var see_all = document.createElement("div"); see_all.classList.add("see_all");
+    see_all.setAttribute('onclick', ''); see_all.innerHTML = "See all tasklists..."
+    document.getElementById('side_bar_tasklists').appendChild(see_all);
+
 }
 
 function countChecked(tasklist_id, child_div, parent_div) {
-    console.log(tasklist_id, child_div, parent_div);
     let count_checked = 0;
     for (let j = 1; j < tasklist_id.length; j++) {
         if (tasklist_id[j].checked) {
@@ -350,4 +423,71 @@ function download(content, fileName, contentType) {
 
 function onDownload() {
     download(JSON.stringify(tasklist), "tasklist.json", "text/plain");
+}
+
+
+// _____________________________________________________________________
+
+function orderBy(list=tasklist[`${current_tl}`], value="checked", order="desc") {
+    temp = list;
+    title = temp.shift();
+    
+    // Sort key = check.
+    if (value == "checked") {
+        temp.sort(function(a, b) {
+            if (a[value] == true && b[value] == false) {
+                if (order == "asc") {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            } else if (a[value] == false && b[value] == true) {
+                if (order == "asc") {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            } else {
+                // return 0;
+                return a.name.localeCompare(b.name);
+            }
+        });
+    }
+
+    // Sort key = alphabetical.
+    if (value == "name") {
+        temp.sort(function(a, b) {
+            if (order == "asc") {
+                return a.name.localeCompare(b.name);
+            } else {
+                return b.name.localeCompare(a.name);
+            }
+        });
+    }
+    temp.unshift(title); list = temp;
+    storageUpdate(tasklist); displayChecklist();
+    return list;
+}
+
+
+function invertValues(id, direction) {
+    if (direction == "up" && id > 1) {
+        let temp_checked = document.getElementById(`checkEdit_${id}`).checked;
+        let temp_value = document.getElementById(`textEdit_${id}`).value;
+
+        document.getElementById(`checkEdit_${id}`).checked = document.getElementById(`checkEdit_${id - 1}`).checked;
+        document.getElementById(`textEdit_${id}`).value = document.getElementById(`textEdit_${id - 1}`).value;
+        
+        document.getElementById(`checkEdit_${id - 1}`).checked = temp_checked;
+        document.getElementById(`textEdit_${id - 1}`).value = temp_value;
+    } else if (direction =="down" && id < (tasklist[`${current_tl}`].length - 1)) {
+        let temp_checked = document.getElementById(`checkEdit_${id}`).checked;
+        let temp_value = document.getElementById(`textEdit_${id}`).value;
+
+        document.getElementById(`checkEdit_${id}`).checked = document.getElementById(`checkEdit_${id + 1}`).checked;
+        document.getElementById(`textEdit_${id}`).value = document.getElementById(`textEdit_${id + 1}`).value;
+        
+        document.getElementById(`checkEdit_${id + 1}`).checked = temp_checked;
+        document.getElementById(`textEdit_${id + 1}`).value = temp_value;
+    }
 }
